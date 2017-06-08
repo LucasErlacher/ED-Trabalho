7f210244efe624c33b1ab783caf724258c77acbe
André Martins <objetovazio@gmail.com>
2017-06-08 20:25:57 -0300

    Merge
    Merge do nodo e da lista.
    Ainda há métodos a adicionar.

---
+++ merge 1.0/lista.c
@@ -0,0 +1,134 @@
+#include "nodo.h"
+#include "lista.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+	Faz a inicialização da LISTA para os valores DEFAULT.
+*/
+lista *criar_lista(){
+	lista* l = malloc(sizeof(lista));
+	l->start = NULL;
+	l->end = NULL;
+	l->size = 0;
+	return l;
+}
+
+/*
+	Retorna o tamanho da lista enviada
+*/
+int get_size(lista *list){
+	return (list->size);
+}
+
+/*
+	Retorna o elem do nodo equivalente ao index da lista
+*/
+void* get_index(lista* list, int index)
+{
+	// Caso lista vazia ou index > size
+	if (!list->size || index >= list->size) {
+		return NULL;
+	}
+	
+	// Percorre a lista até pegar o index desejado
+	nodo *current = list->start; // index = 0
+	int i;
+	for (i = 0; i<index; i++) {
+		current = current->next;
+	}
+		
+	return current->elem;
+}
+
+/*
+	Retorna o nodo equivalente ao index da lista
+*/
+nodo* _get_nodo_index(lista* list, int index)
+{
+	// Caso lista vazia ou index > size
+	if (!list->size || index >= list->size) {
+		return NULL;
+	}
+	
+	// Percorre a lista até pegar o index desejado
+	nodo *current = list->start; // index = 0
+	int i;
+	for (i = 0; i<index; i++) {
+		current = current->next;
+	}
+		
+	return current;
+}
+
+/*
+	Insere um elemento qualquer no início da LISTA.
+*/
+void inserir_start(lista* list, void* elem){
+	nodo *n = criar_nodo(elem);
+
+	// Se tamanho da lista for 0
+	if (!list->size) {
+		// então o ultimo elemento da lista é n
+		list->end = n;
+	} 
+	else {
+		nodo *start = list->start;	// Pega primeiro nodo
+		start->prev = n; // Seta nodo anterior de start como N
+		n->next = start; // Seta o nodo seguinte de N como start
+	}
+	
+	list->start = n; // Seta primeiro elemento da lista como N
+	list->size++; // Incrementa Lista
+	return;
+}
+
+/*
+	Insere o NODO ao final da LISTA.
+*/
+void inserir(lista* list, void* elem){
+	nodo *n = criar_nodo(elem);
+
+	// Se tamanho da lista for 0
+	if (!list->size) {
+		// Já que não há outros nós, esse será o inicial;
+		list->start = n;
+	} 
+	else {
+		nodo *end = list->end; // Pega ultimo nodo
+		end->next = n; // Seta proximo nodo de END como N
+		n->prev = end; // Seta anterior de N como END
+	}
+
+	list->end = n; // Seta N como nó final da lista
+	list->size++; // Incrementa tamanho
+}
+
+/*
+	Insere elemento no index enviado como paramentro
+*/
+void inserir_index(lista* list, void* elem, int index){
+	if(index == 0) inserir_start(list, elem); // Caso index seja 0, usa a função insere_start
+	else if(index >= list->size) inserir(list, elem); // Caso o index seja maior ou igual ao tamanho da lista, insere no final
+	else{
+		nodo *novo = criar_nodo(elem);
+		nodo* cur = _get_nodo_index(list, index);
+		
+		novo->next = cur; // 
+		novo->prev = cur->prev;
+		
+		(novo->prev)->next = novo;
+		
+		cur->prev = novo;
+		(list->size)++;		
+	}
+}
+
+void remover_final(lista* list){
+	nodo *end = list->end;
+	list->end = end->prev;
+	//free(end->elem);
+	free(end);
+	(list->end)->next = NULL;
+	return;
+}
---
+++ merge 1.0/lista.h
@@ -0,0 +1,32 @@
+#ifndef LISTA_H_
+#define LISTA_H_
+#include "nodo.h"
+
+typedef struct lista
+{
+	nodo* start;
+	nodo* end;
+	unsigned int size; /* Tamanho da lista */
+} lista;
+
+/* Inicio Criação da lista */
+lista* criar_lista();
+/* Inicio Criação da lista */
+
+/* Inicio Consulta */
+int get_size(lista *lst);
+void* get_index(lista* list, int index); 
+nodo* get_nodo_index(lista* list, int index);
+/* Fim Consulta */
+
+/* Inicio Adicionar Elementos */
+void inserir_start(lista* list, void* elem);
+void inserir(lista* list, void* elem);
+void inserir_index(lista* list, void* elem, int index);
+/* Fim Adicionar Elementos */
+
+/* Inicio Remover Elementos */
+void remover_final(lista* list);
+/* Fim Remover Elementos */
+
+#endif
